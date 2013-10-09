26eed8e5ace631a9fdf52d649a4f2884b119b50d
jiangqq@163.com <jiangqingqing90>
2013-10-09 14:46:29 +0800

    项目导入:

---
+++ TruthAdventure/src/com/pps/truthadventure/activity/LogoActivity.java
@@ -0,0 +1,13 @@
+package com.pps.truthadventure.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.logo);
+	}
+}
