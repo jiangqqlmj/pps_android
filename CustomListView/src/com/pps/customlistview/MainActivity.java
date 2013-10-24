d70b2970910ad2ec04dafa75399bffd65c0671d0
jiangqq@163.com <jiangqingqing90>
2013-10-24 17:33:52 +0800

    自定义ListView ,带有边框

--- CustomListView/src/com/pps/customlistview/MainActivity.java
+++ CustomListView/src/com/pps/customlistview/MainActivity.java
@@ -2,6 +2,8 @@ package com.pps.customlistview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 
 public class MainActivity extends Activity {
     
@@ -15,6 +17,21 @@ public class MainActivity extends Activity {
 		String[] mStrs=new String[]{"张三","李四","王五","赵六"};
 		mCustomAdapter=new CustomAdapter(this, mStrs);
 		lv_custom.setAdapter(mCustomAdapter);
+		lv_custom.setOnScrollListener(new OnScrollListener() {
+			
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		
 	}
