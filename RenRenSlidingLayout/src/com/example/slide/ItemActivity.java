51086afceb8f685991c8c1546a1d612eb222fba8
jiangqq@163.com <jiangqingqing90>
2013-11-05 18:31:55 +0800

    侧滑界面demo

---
+++ RenRenSlidingLayout/src/com/example/slide/ItemActivity.java
@@ -0,0 +1,12 @@
+package com.example.slide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItemActivity extends Activity {
+ @Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.item);
+}
+}
